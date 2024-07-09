b52ad1a680da784dc2fc7c6284ef3e18124f14e4
himan1009 <shishusha922@gmail.com>
2024-07-09 15:07:57 +0530

    server fromation

---
+++ index.js
@@ -0,0 +1,20 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    if(req.url==="/about"){
+        res.end("<h1>ABOUT PAGE</h1>")
+    }
+    else if(req.url==="/"){
+        res.end("<h1>HOME PAGE</h1>");
+    }
+    else if(req.url==="/contact"){
+        res.end("<h1>CONTACT PAGE</h1>");
+    }
+    else{
+        res.end("<h1>PAGE NOT FOUND</h1>");
+    }
+});
+
+server.listen(5000, () => {
+  console.log("server is wroking");
+});
