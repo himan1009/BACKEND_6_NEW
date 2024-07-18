b350d8cff1fbaf25f143c972be98ca311248d1d9
himan1009 <shishusha922@gmail.com>
2024-07-18 17:33:49 +0530

    new

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import express from 'express';
+import fs from 'fs';
+import path from 'path';
 
 const app=express();
 
 app.get("/getproducts", (req, res)=>{
-    res.json({
-        success:true,
-        products:[],
-    })
+    const pathLocation=path.resolve();
+    res.sendFile(path.join(pathLocation, "./index.html"));
 })
 
 app.listen(5000, ()=>{
