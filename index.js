4654cebd28cb564e79d4eac363b83f7b6a749bb0
himan1009 <shishusha922@gmail.com>
2024-07-18 17:25:15 +0530

    add

--- index.js
+++ index.js
@@ -1,31 +1,14 @@
-import http from "http";
-import {gfName, gfName1, love} from "./features.js";
-import fs from "fs";
-import path from "path";
+import express from 'express';
 
-const home=fs.readFileSync("./index.html");
+const app=express();
 
-console.log(path.extname("/home/random/index.html"));
-console.log(gfName);
-console.log(gfName1);
+app.get("/getproducts", (req, res)=>{
+    res.json({
+        success:true,
+        products:[],
+    })
+})
 
-const server = http.createServer((req, res) => {
-    if(req.url==="/about"){
-        res.end(`<h1>LOVE IS ${love()}</h1>`)
-    }
-    else if(req.url==="/"){
-        fs.readFile("./index.html", (err, home)=>{
-            res.end(home);
-        });
-    }
-    else if(req.url==="/contact"){
-        res.end("<h1>CONTACT PAGE</h1>");
-    }
-    else{
-        res.end("<h1>PAGE NOT FOUND</h1>");
-    }
-});
-
-server.listen(5000, () => {
-  console.log("server is wroking");
-});
+app.listen(5000, ()=>{
+    console.log("server is working");
+})
